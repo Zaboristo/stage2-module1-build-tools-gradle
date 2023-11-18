863709e088affd46afd7b1e62838d799f9cf3fa5
boris <baizenbe@gmail.com>
2023-11-18 15:36:18 +0600

    gradled

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -7,7 +7,8 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        if (args.contains(null)) return false;
+        if (args.contains(null) || args == null) return false;
+
 
         for(String str: args){
             if (str.startsWith("-") || str.equals("0") || str.startsWith(" ") || str.equals("")) return false;
