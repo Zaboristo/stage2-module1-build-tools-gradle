4089172b7225289da3626fa2542ec9d45f54b58c
boris <baizenbe@gmail.com>
2023-11-18 15:26:58 +0600

    gradled

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,21 @@
 package com.epam.demo;
 
+
+
+import com.epam.utils.StringUtils;
+
 import java.util.List;
+
+
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+
+        for(String str: args){
+            if (!StringUtils.isPositiveNumber(str)) return false;
+            
+        }
+        return true;
         //magic happens here
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,9 @@
 package com.epam.utils;
 
+
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        return (!(str.startsWith("-") || str.equals(null) || str.startsWith(" ") || str.equals("")));
     }
 }
